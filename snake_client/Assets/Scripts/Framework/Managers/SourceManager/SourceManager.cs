18efd9a5dc44dc58c251be310dfa80f647d61425
ClaineLe <7941324@qq.com>
2020-03-14 02:47:47 +0800

    增加同步加载资源接口（临时）

--- snake_client/Assets/Scripts/Framework/Managers/SourceManager/SourceManager.cs
+++ snake_client/Assets/Scripts/Framework/Managers/SourceManager/SourceManager.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SourceManager : MonoBehaviour
+public class SourceManager : Singleton<SourceManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Init()
     {
-        
+        base.Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadAsset<T>(string assetPath, System.Action<T> callback) where T : Object
     {
-        
+        T assetObj = Resources.Load<T>(assetPath);
+        callback?.Invoke(assetObj);
     }
 }
