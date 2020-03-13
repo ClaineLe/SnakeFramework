1b10874f7f782f14581e805a66e8d4c01715bb56
ClaineLe <7941324@qq.com>
2020-03-14 02:04:10 +0800

    增加controller目录，所有controller都是非静态对象需要被释放(Dispose or Release or Destroy)

---
+++ snake_client/Assets/Scripts/Framework/Controllers/PatchController/PatchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatchController : IController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
