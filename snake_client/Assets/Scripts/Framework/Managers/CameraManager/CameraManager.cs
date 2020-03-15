e0d570b76715b79bef02d89c2c415379032a2412
ClaineLe <7941324@qq.com>
2020-03-15 14:32:10 +0800

    +摄像机管理器

---
+++ snake_client/Assets/Scripts/Framework/Managers/CameraManager/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : Singleton<CameraManager>
+{
+    private Camera mMainCamera;
+    public override void Init()
+    {
+        base.Init();
+        this.mMainCamera = Camera.main;
+        GameObject.DontDestroyOnLoad(this.mMainCamera);
+    }
+
+
+}
