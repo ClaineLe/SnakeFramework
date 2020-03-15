191a9b573f4d490e2363eb127603624c5d33b995
ClaineLe <7941324@qq.com>
2020-03-15 20:30:08 +0800

    +UI框架，子界面节点的扩展

---
+++ snake_client/Assets/Scripts/Framework/Managers/UIManager/SubScreenBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubScreenBase
+{
+    protected UISubCtrlBase mCtrlBase;
+
+    public UISubCtrlBase CtrlBase { get => this.mCtrlBase; }
+
+    public SubScreenBase(UISubCtrlBase ctrlBase)
+    {
+        this.mCtrlBase = ctrlBase;
+        Init();
+    }
+
+    virtual protected void Init()
+    {
+    }
+
+    virtual public void Dispose()
+    {
+
+    }
+}
