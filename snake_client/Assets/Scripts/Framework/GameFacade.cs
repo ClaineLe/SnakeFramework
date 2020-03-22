2497f7372b839d2e4e1bc4fece7b6f406432792f
ClaineLe <7941324@qq.com>
2020-03-22 22:52:04 +0800

    Lua侧中加入游戏管理器门户类

---
+++ snake_client/Assets/Scripts/Framework/GameFacade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFacade : Singleton<GameFacade>
+{
+    public DataManager mDataMgr
+    {
+        get
+        {
+            if (this._dataMgr == null)
+                this._dataMgr = Singleton<DataManager>.GetInstance();
+            return this._dataMgr;
+        }
+    }
+    private DataManager _dataMgr = null;
+    
+    public UIManager mUiMgr
+    {
+        get
+        {
+            if (this._uiMgr == null)
+                this._uiMgr = Singleton<UIManager>.GetInstance();
+            return this._uiMgr;
+        }
+    }
+    private UIManager _uiMgr = null;
+}
